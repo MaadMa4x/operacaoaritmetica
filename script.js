30e9be77125b0471e5a821b3d9357cdf55d8aeb8
Gabrielly <164526851+MaadMa4x@users.noreply.github.com>
2024-06-14 12:32:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+/*var a = 10;
+var b = 15;ie=edge
+
+var c = a + b;
+
+console.log(c) ;
+ie=edge
+var a = 10;
+var b = 15;
+
+var c = a - b;
+
+console.log(c) ;
+
+var a = 10;
+var b = 15;
+
+console.log(c) ;
+
+var a = 10;
+var b = 15;
+
+var c = a / b;
+
+console.log(c) ;
+
+var a = 10;
+var b = 15;
+
+var c = a % b;
+
+console.log(c) ;
+
+var
+var a = 10;
+var b = 15;
+
+var c = a ++ a;
+
+console.log(c) ;
+
+var a = 10;
+var b = 15;
+
+var c = a ++ b;
+
+console.log(c);*/
